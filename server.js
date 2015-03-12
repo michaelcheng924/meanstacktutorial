36dd270303e534d612537915fdfbe7dcb220952e
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-11 22:27:22 -0500

    change tabs to spaces in server.js

--- server.js
+++ server.js
@@ -6,49 +6,51 @@ var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
 var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.get('/contactlist', function (req, res) {
-	console.log("I received a GET request")
+  console.log('I received a GET request');
 
-	db.contactlist.find(function (err, docs) {
-		console.log(docs);
-		res.json(docs);
-	});
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
 });
 
 app.post('/contactlist', function (req, res) {
-	console.log(req.body);
-	db.contactlist.insert(req.body, function(err, doc) {
-		res.json(doc);
-	})
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
 });
 
 app.delete('/contactlist/:id', function (req, res) {
-	var id = req.params.id;
-	console.log(id);
-	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
-		res.json(doc);
-	})
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
 });
 
 app.get('/contactlist/:id', function (req, res) {
-	var id = req.params.id;
-	console.log(id);
-	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
-		res.json(doc);
-	});
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
 });
 
 app.put('/contactlist/:id', function (req, res) {
-	var id = req.params.id;
-	console.log(req.body.name);
-	db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
-		update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
-		new: true}, function (err, doc) {
-			res.json(doc);
-		});
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.contactlist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
 });
 
 app.listen(3000);
