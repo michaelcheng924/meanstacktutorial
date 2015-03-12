3e75fa70a0594d0b6843783e41f944fba3867ac4
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-11 22:30:17 -0500

    change tabs to spaces in index and controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,46 +4,46 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
 
 var refresh = function() {
-	$http.get('/contactlist').success(function(response) {
-		console.log("I got the data I requested");
-		$scope.contactlist = response;
-		$scope.contact = "";
-	});
+  $http.get('/contactlist').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.contactlist = response;
+    $scope.contact = "";
+  });
 };
 
 refresh();
 
 $scope.addContact = function() {
-	console.log($scope.contact);
-	$http.post('/contactlist', $scope.contact).success(function(response) {
-		console.log(response);
-		refresh();
-	});
+  console.log($scope.contact);
+  $http.post('/contactlist', $scope.contact).success(function(response) {
+    console.log(response);
+    refresh();
+  });
 };
 
 $scope.remove = function(id) {
-	console.log(id);
-	$http.delete('/contactlist/' + id).success(function(response) {
-		refresh();
-	});
+  console.log(id);
+  $http.delete('/contactlist/' + id).success(function(response) {
+    refresh();
+  });
 };
 
 $scope.edit = function(id) {
-	console.log(id);
-	$http.get('/contactlist/' + id).success(function(response) {
-		$scope.contact = response;
-	});
-};	
+  console.log(id);
+  $http.get('/contactlist/' + id).success(function(response) {
+    $scope.contact = response;
+  });
+};  
 
 $scope.update = function() {
-	console.log($scope.contact._id);
-	$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
-		refresh();
-	})
+  console.log($scope.contact._id);
+  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+    refresh();
+  })
 };
 
 $scope.deselect = function() {
-	$scope.contact = "";
+  $scope.contact = "";
 }
 
 }]);﻿
