0898068e34f6fb2bd9afca9483ed59c549428be2
yamilelias <yamileliassoto@gmail.com>
2017-01-29 17:52:48 -0700

    Comment all console log lines

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,14 +7,14 @@ var myApp = angular.module('myApp', []);
  * myApp controller
  */
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+    // console.log("Hello World from controller");
 
     /**
      * Retrieve the contacts again
      */
     var refresh = function() {
         $http.get('/contactlist').success(function(response) {
-            console.log("I got the data I requested");
+            // console.log("I got the data I requested");
             $scope.contactlist = response;
             $scope.contact = "";
         });
@@ -25,9 +25,9 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
      * Add contact to database
      */
     $scope.addContact = function() {
-    console.log($scope.contact);
+    // console.log($scope.contact);
         $http.post('/contactlist', $scope.contact).success(function(response) {
-            console.log(response);
+            // console.log(response);
             refresh();
     });
     };
@@ -37,7 +37,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
      * @param id
      */
     $scope.remove = function(id) {
-        console.log(id);
+        // console.log(id);
         $http.delete('/contactlist/' + id).success(function(response) {
             refresh();
         });
@@ -48,7 +48,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
      * @param id
      */
     $scope.edit = function(id) {
-        console.log(id);
+        // console.log(id);
         $http.get('/contactlist/' + id).success(function(response) {
             $scope.contact = response;
         });
@@ -58,7 +58,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
      * Update a contact information
      */
     $scope.update = function() {
-            console.log($scope.contact._id);
+            // console.log($scope.contact._id);
             $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
                 refresh();
             })
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/contactlist', function (req, res) {
   console.log('I received a GET request');
 
   db.contactlist.find(function (err, docs) {
-    console.log(docs);
+    // console.log(docs);
     res.json(docs);
   });
 });
@@ -37,7 +37,7 @@ app.get('/contactlist', function (req, res) {
  * POST Method to update a contact
  */
 app.post('/contactlist', function (req, res) {
-  console.log(req.body);
+  // console.log(req.body);
   db.contactlist.insert(req.body, function(err, doc) {
     res.json(doc);
   });
@@ -48,7 +48,7 @@ app.post('/contactlist', function (req, res) {
  */
 app.delete('/contactlist/:id', function (req, res) {
   var id = req.params.id;
-  console.log(id);
+  // console.log(id);
   db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
@@ -59,7 +59,7 @@ app.delete('/contactlist/:id', function (req, res) {
  */
 app.get('/contactlist/:id', function (req, res) {
   var id = req.params.id;
-  console.log(id);
+  // console.log(id);
   db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
@@ -70,7 +70,7 @@ app.get('/contactlist/:id', function (req, res) {
  */
 app.put('/contactlist/:id', function (req, res) {
   var id = req.params.id;
-  console.log(req.body.name);
+  // console.log(req.body.name);
   db.contactlist.findAndModify({
     query: {_id: mongojs.ObjectId(id)},
     update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
@@ -81,4 +81,4 @@ app.put('/contactlist/:id', function (req, res) {
 });
 
 app.listen(3000);
-console.log("Server running on port 3000");
+// console.log("Server running on port 3000");
